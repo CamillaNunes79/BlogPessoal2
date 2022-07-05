b85a24893df9685cda9f150c96a5b8b7d3e4a87e
unknown <Camilla Nunes>
2022-07-05 01:11:50 -0300

    consumindo api

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
+Usuario: Usuario = new Usuario
+confirmarSenha: string
+tipoUser: string
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any) {
+this.confirmarSenha = event.target.value
+  }
+
+tipoUsuario(event: any) {
+this.tipoUser = event.target.value
+}
+
+cadastrar() {
+this.Usuario.tipo = this.tipoUsuario
+}
 
 }
